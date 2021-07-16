17fd102bc24cb2fd1d1785c95b362b4f4066c2f7
fauverick <hungnc888@gmail.com>
2021-07-16 21:40:31 +0700

    deranger

--- derangerlab_new/js/index.js
+++
@@ -1,7 +0,0 @@
-let pathname = location.pathname;
-console.log(pathname);
-
-let link1 = document.getElementById("l1");
-if(pathname == "file:///E:/Web%20Frontend/Deranger/lookbook/index.html"){
-    link1.style.color = "#d32521";
-}
